fd902f986683bbdd6083da19ef3a903c73e0bc44
1iNi1 <nayir94@hotmail.fr>
2020-08-06 06:16:01 +0200

    main-sections update

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Main.css'
+import './Main-sections.css'
 export default function Main() {
     return (
         <div className="main">
@@ -13,10 +14,21 @@ export default function Main() {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perspiciatis quis asperiores, enim esse deleniti iste harum hic accusamus reprehenderit quaerat repellat optio laboriosam repellendus ab ipsam earum possimus. Labore quos quaerat at corporis esse! Asperiores, qui labore quia ratione tenetur obcaecati sint, accusamus excepturi quibusdam fuga deleniti nihil. Repellat?</p>
             </div>
             <div className="main-sections">
-                <div className="main-section courses"><img src={require("./images/field.jpg")} alt="field"/></div>
-                <div className="main-section students"><img src={require("./images/shooting.jpg")} alt="shooting"/></div>
-                <div className="main-section videos"><img src={require("./images/street_game.jpg")} alt="street game"/></div>
-                <div className="main-section blog"><img src={require("./images/ball.jpg")} alt="ball"/></div>
+                <div class="main-sections-front" id="front">
+                    <div class="main-section courses" id="courses"></div>
+                    <div class="main-section students" id="students"></div>
+                    <div class="main-section videos" id="videos"></div>
+	                <div class="main-section blog" id="blog"></div>
+                </div>
+                <div class="main-sections-back" id="back">
+                    <div class="main-section-info" id="info">
+	                    <h3 id="info-title">Blabla</h3>
+	                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit autem totam provident ullam reiciendis labore quaerat ad? Explicabo, eius provident.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit autem totam provident ullam reiciendis labore quaerat ad? Explicabo, eius provident.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit autem totam provident ullam reiciendis labore quaerat ad? Explicabo, eius provident.
+	                    </p>
+	                    <a href="/" id="info-link">Click here!</a>
+                    </div>
+
+                </div>
             </div>
             <div className="main-form">
                 <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, corrupti!</h2>
