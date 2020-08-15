cb40dd51bf51a8a4beb95203102d0539a0760d28
1iNi1 <nayir94@hotmail.fr>
2020-08-15 05:42:50 +0200

    adding pages folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home'
+import Courses from './components/Pages/Courses/Courses' 
+import Students from './components/Pages/Students/Students'
+import Videos from './components/Pages/Videos/Videos'
+import Blog from './components/Pages/Blog/Blog'
+import About from './components/Pages/About/About'
+import Login from './components/Pages/Login/Login'
+import ErrorPage from './components/Pages/ErrorPage/ErrorPage'
+
 import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Home}/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/Courses" component={Courses}/>
+          <Route exact path="/Students" component={Students}/>
+          <Route exact path="/Videos" component={Videos}/>
+          <Route exact path="/Blog" component={Blog}/>
+          <Route exact path="/About" component={About}/>
+          <Route exact path="/Login" component={Login}/>
+          <Route component={ErrorPage}/>
+        </Switch>
       </div>
     </Router>
   );
