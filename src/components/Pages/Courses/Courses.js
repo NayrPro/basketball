89672d179066bac32489f61d5584abdcdd4c2ae5
1iNi1 <nayir94@hotmail.fr>
2020-08-21 11:30:27 +0200

    website first version

--- src/components/Pages/Courses/Courses.js
+++ src/components/Pages/Courses/Courses.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./courses.css"
 import Navbar from '../../Navbar/Navbar'
+import {Link} from "react-router-dom"
 
 export default function Courses() {
-    /* Will contain all the courses element */
-    var courses = []
+    const [page, setPage] = useState(1)
 
-    function coursesPush(){
-        for(let i = 0; i<45; i++){
-            courses.push(
-                <div className="course">
-                    <img src={require("./course1.jpg")} alt={`course ${i+1}`}/>
+    /* Will contain all the courses element */
+    const [courses, setCourses] = useState()
+
+    function coursesPush(page){
+        var array = []
+        for(let i = ((page*15)-14); i<=(page*15); i++){
+            array.push(
+                <div className="course" key={i}>
+                    <img src={require("./course1.jpg")} alt={`course ${i}`}/>
                     <div className="course-description">
-                        <a href="/">Course {i+1}</a>
+                        <Link to={`/Courses/${i}`}>Course {i}</Link>
                         <hr/>
                         <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, voluptatem?</span>
                     </div>
                 </div>
             )
         }
+        setCourses(array)
     } 
     
-    coursesPush();
+    useEffect(() => {
+        coursesPush(page)
+    }, [page])
+
+    function handleChange(e){
+        const value = e.target.value
+        setPage(value)
+        console.log(page);
+    }
 
     return (
         <React.Fragment>
@@ -34,8 +47,8 @@ export default function Courses() {
                 </div>
                 <div className="courses-pages">
                     <div className="courses-pages container">
-                        <select name="pages" id="page-select">
-                            <option value="">--Pages--</option>
+                        <select name="pages" id="page-select" onChange={(e) => handleChange(e)} value={page}>
+                            {/* <label>--Pages--</label> */}
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
