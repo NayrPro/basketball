619e904ec6bdf76d3c6611e07491a413dc7055e0
1iNi1 <nayir94@hotmail.fr>
2020-09-01 17:06:54 +0200

    adding videoModal

--- src/components/Pages/Videos/Videos.js
+++ src/components/Pages/Videos/Videos.js
@@ -1,34 +1,101 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./videos.css"
 import videosData from './videosData'
 
 export default function Videos() {
 
+    /* To show or hide the specific video clicked on */
+    
+    const [display, setDisplay] = useState("none")
+    const [url, setUrl] = useState("")
+    const [autoplay, setAutoplay] = useState("") // Pauses the video when you quit it
+    const [index, setIndex] = useState(0) // Allows us to get access to a video based on his index in videosData
+    
+    function videoModal(urlValue, keyValue){
+        setIndex(keyValue-1)
+        setAutoplay("")
+        setDisplay("block") 
+        setUrl(urlValue)
+    }
+
     /* Stocks the title and the images of each
         video inside JSX elements */
     const videos = videosData.map(video => (
         <div className="video" key={video.key}>
-                        <a href="/" className="video-thumbnail">
-                        <img 
-                        src= {require(`${video.urlImg}`)}
-                        onMouseEnter={(e) => e.target.src = require(`${video.urlGif}`)}
-                        onMouseLeave={(e) => e.target.src = require(`${video.urlImg}`)}
-                         alt={video.key}/>
-                        <i className="fas fa-play"></i>
+                        <a 
+                            href="/" 
+                            className="video-thumbnail" 
+                            onClick={(e) =>{
+                                e.preventDefault()
+                                videoModal(video.url, video.key)
+                            }}
+                        >
+                            <img 
+                                src= {require(`${video.urlImg}`)}
+                                onMouseEnter={(e) => e.target.src = require(`${video.urlGif}`)}
+                                onMouseLeave={(e) => e.target.src = require(`${video.urlImg}`)}
+                                alt={video.key}/>
+                            <i className="fas fa-play"></i>
                         </a>
-                        <div className="video-description">
+
+                        <div 
+                            className="video-description" onClick={(e) =>{
+                                e.preventDefault()
+                                videoModal(video.url, video.key)}}>
                             <a href="/">{video.title}</a>
                         </div>
         </div>
     ))
+    
     return (
         <React.Fragment>
-            <div className="courses-container">
+            <div className="video-overlay" style={{display: display}}>
+                <i 
+                    className="fas fa-times" 
+                    onClick={() => {
+                        setDisplay("none")
+                        setAutoplay("?autoplay=0")
+                    }}
+                ></i>
+                <iframe 
+                    title="video" 
+                    width="800" 
+                    height="506" 
+                    src={"https://www.youtube.com/embed/"+url+autoplay} 
+                    frameBorder="0" 
+                    allowFullScreen={true}
+                >
+                </iframe>
+                <div className="chevron">
+                    <i 
+                        className={index <= 0 ? "fas fa-chevron-left arrow-transparent" : "fas fa-chevron-left"} 
+                        aria-hidden="true"
+                        onClick={() => {
+                            if(index > 0){
+                                setUrl(videosData[index-1].url) 
+                                setIndex(index-1)
+                            }
+                        }}
+                    ></i>
+                    <i 
+                        className={index < videosData.length-1 ? "fas fa-chevron-right" : "fas fa-chevron-right arrow-transparent"}
+                         aria-hidden="true"
+                         onClick={() => {
+                            if(index < videosData.length-1){ 
+                                setUrl(videosData[index+1].url)
+                                setIndex(index+1)
+                            }
+                        }}
+                    ></i>
+                </div>
+            </div>
+            <div className="courses-container" style={{opacity: display === "block" ? "0.5" : "1"}}>
                 <div className="videos-banner">
                     <div className="videos-banner overlay">
                         <h2>Videos</h2>
                     </div>
                 </div>
+
                 <div className="videos-grid">
                     {/*Displaying each video's infos*/}
                     {videos} 
--- src/components/Pages/Videos/videosData.js
+++ src/components/Pages/Videos/videosData.js
@@ -1,80 +1,113 @@
 const videosData = [
-    {
-        "key": 1, 
-        "urlImg": "./Videos/images/gif1.jpg",
-        "urlGif": "./Videos/gif/gif1.gif",
-        "title": "Sed si ille hac tam eximia fortuna propter"
-      },
-      { "key": 2,  
-        "urlImg": "./Videos/images/gif2.jpg",
-        "urlGif": "./Videos/gif/gif2.gif",
-        "title": "Quanta autem vis sit, ex hoc intellegi"
-      },
-      { "key": 3,
-        "urlImg": "./Videos/images/gif3.jpg",
-        "urlGif": "./Videos/gif/gif3.gif",
-        "title": "Excitavit hic ardor milites per municipia plurima"
-      },
-      { "key": 4,
-        "urlImg": "./Videos/images/gif4.jpg",
-        "urlGif": "./Videos/gif/gif4.gif",
-        "title": "Illud dubitatur cum esset aliquando"
-      },
-      { "key": 5,
-        "urlImg": "./Videos/images/gif5.jpg",
-        "urlGif": "./Videos/gif/gif5.gif",
-        "title": "Itaque tum Scaevola cum in eam ipsam mentionem"
-      },
-      { "key": 6,
-        "urlImg": "./Videos/images/gif6.jpg",
-        "urlGif": "./Videos/gif/gif6.gif",
-        "title": "Et quoniam mirari posse peregrinos existimo"
-      },
-      { "key": 7,
-        "urlImg": "./Videos/images/gif7.jpg",
-        "urlGif": "./Videos/gif/gif7.gif",
-        "title": "Et quia Montius dilan manus spiritum"
-      },
-      { "key": 8,
-        "urlImg": "./Videos/images/gif8.jpg",
-        "urlGif": "./Videos/gif/gif8.gif",
-        "title": "Homines enim eruditos et sobrios ut infaustos"
-      },
-      { "key": 9,
-        "urlImg": "./Videos/images/gif9.jpg",
-        "urlGif": "./Videos/gif/gif9.gif",
-        "title": "Martinus agens illas provincias pro praefectis"
-      },
-      { "key": 10,
-        "urlImg": "./Videos/images/gif10.jpg",
-        "urlGif": "./Videos/gif/gif10.gif",
-        "title": "Abusus enim multitudine hominum, tranquillis"
-      },
-      { "key": 11,
-        "urlImg": "./Videos/images/gif11.jpg",
-        "urlGif": "./Videos/gif/gif11.gif",
-        "title": "Sed quid est quod in hac causa"
-      },
-      { "key": 12,
-        "urlImg": "./Videos/images/gif12.jpg",
-        "urlGif": "./Videos/gif/gif12.gif",
-        "title": "Quibus ita patratis Paulus"
-      },
-      { "key": 13,
-        "urlImg": "./Videos/images/gif13.jpg",
-        "urlGif": "./Videos/gif/gif13.gif",
-        "title": "Utque proeliorum periti rectores primo"
-      },
-      { "key": 14,
-        "urlImg": "./Videos/images/gif14.jpg",
-        "urlGif": "./Videos/gif/gif14.gif",
-        "title": "Illud autem non dubitatur quod cum"
-      },
-      { "key": 15,
-        "urlImg": "./Videos/images/gif15.jpg",
-        "urlGif": "./Videos/gif/gif15.gif",
-        "title": "Mox dicta finierat, multitudo omnis ad"
-      }
+  {
+    "key": 1, 
+    "urlImg": "./Videos/images/gif1.jpg",
+    "urlGif": "./Videos/gif/gif1.gif",
+    "url": "kxIPbDmNfxE",
+    "title": "Sed si ille hac tam eximia fortuna propter"
+  },
+  { 
+    "key": 2,  
+    "urlImg": "./Videos/images/gif2.jpg",
+    "urlGif": "./Videos/gif/gif2.gif",
+    "url": "V51C3SJC57U",
+    "title": "Quanta autem vis sit, ex hoc intellegi"
+  },
+  { 
+    "key": 3,
+    "urlImg": "./Videos/images/gif3.jpg",
+    "urlGif": "./Videos/gif/gif3.gif",
+    "url": "xVQk71wTF88",
+    "title": "Excitavit hic ardor milites per municipia plurima"
+  },
+  { 
+    "key": 4,
+    "urlImg": "./Videos/images/gif4.jpg",
+    "urlGif": "./Videos/gif/gif4.gif",
+    "url": "gYxVcfaGih8",
+    "title": "Illud dubitatur cum esset aliquando"
+  },
+
+  { 
+    "key": 5,
+    "urlImg": "./Videos/images/gif5.jpg",
+    "urlGif": "./Videos/gif/gif5.gif",
+    "url": "L3qJFjlDvKY",
+    "title": "Itaque tum Scaevola cum in eam ipsam mentionem"
+  },
+  { 
+    "key": 6,
+    "urlImg": "./Videos/images/gif6.jpg",
+    "urlGif": "./Videos/gif/gif6.gif",
+    "url": "k3es0eSgOfU",
+    "title": "Et quoniam mirari posse peregrinos existimo"
+  },
+  { 
+    "key": 7,
+    "urlImg": "./Videos/images/gif7.jpg",
+    "urlGif": "./Videos/gif/gif7.gif",
+    "url": "d74TkJIWqZE",
+    "title": "Et quia Montius dilan manus spiritum"
+  },
+  { 
+    "key": 8,
+    "urlImg": "./Videos/images/gif8.jpg",
+    "urlGif": "./Videos/gif/gif8.gif",
+    "url": "OGDJrHd3PZo",
+    "title": "Homines enim eruditos et sobrios ut infaustos"
+  },
+  { 
+    "key": 9,
+    "urlImg": "./Videos/images/gif9.jpg",
+    "urlGif": "./Videos/gif/gif9.gif",
+    "url": "1Sa9Zaf_OZk",
+    "title": "Martinus agens illas provincias pro praefectis"
+  },
+  { 
+    "key": 10,
+    "urlImg": "./Videos/images/gif10.jpg",
+    "urlGif": "./Videos/gif/gif10.gif",
+    "url": "f4exubds_hE",
+    "title": "Abusus enim multitudine hominum, tranquillis"
+  },
+  { 
+    "key": 11,
+    "urlImg": "./Videos/images/gif11.jpg",
+    "urlGif": "./Videos/gif/gif11.gif",
+    "url": "LGmVrDB8iGg",
+    "title": "Sed quid est quod in hac causa"
+  },
+  { 
+    "key": 12,
+    "urlImg": "./Videos/images/gif12.jpg",
+    "urlGif": "./Videos/gif/gif12.gif",
+    "url": "MhPdwzDKOb0",
+    "title": "Quibus ita patratis Paulus"
+  },
+  { 
+    "key": 13,
+    "urlImg": "./Videos/images/gif13.jpg",
+    "urlGif": "./Videos/gif/gif13.gif",
+    "url": "O3lw6kM2_QY",
+    "title": "Utque proeliorum periti rectores primo"
+  },
+  { 
+    "key": 14,
+    "urlImg": "./Videos/images/gif14.jpg",
+    "urlGif": "./Videos/gif/gif14.gif",
+    "url": "OpbXeMo0O-U",
+    "title": "Illud autem non dubitatur quod cum"
+  },
+  { 
+    "key": 15,
+    "urlImg": "./Videos/images/gif15.jpg",
+    "urlGif": "./Videos/gif/gif15.gif",
+    "url": "QjqA8QVCBvU",
+    "title": "Mox dicta finierat, multitudo omnis ad"
+  }
 ]
 
 export default videosData
+
+
+
