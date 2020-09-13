86c52e08a866c47a8e6fd43650deb859ec44451b
NayrPro <nayir.a.pro@gmail.com>
2020-09-14 00:59:06 +0200

    minor changes

--- src/components/Home/Main/components/Headline.js
+++ src/components/Home/Main/components/Headline.js
@@ -7,7 +7,7 @@ export default function Headline() {
             <h1 className="main-title">LOREM IPSUM DOLOR!</h1>
             <p className="main-title-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perspiciatis quis asperiores, enim esse deleniti iste harum hic accusamus reprehenderit quaerat repellat optio laboriosam repellendus ab ipsam earum possimus. Labore quos quaerat at corporis esse! Asperiores.<Link to="/" replace>Click Here!</Link></p>
             <div className="main-overlay">
-                <img className="main-image" src={require('../images/home.jpg')} alt="main"></img>
+                <img className="main-image" src={require('../images/home.jpg')} alt="main" height="656.094"></img>
             </div>
         </React.Fragment>
     )
